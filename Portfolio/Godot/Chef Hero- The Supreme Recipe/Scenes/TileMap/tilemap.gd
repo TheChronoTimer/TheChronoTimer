9302bb684ac34b8c564a930b30a492ab583ebe32
TheChronoTimer <nicolas_bauermann@tutanota.com>
2024-10-28 22:49:15 +0000

    .

--- Portfolio/Godot/Chef Hero- The Supreme Recipe/Scenes/TileMap/tilemap.gd
+++ Portfolio/Godot/Chef Hero- The Supreme Recipe/Scenes/TileMap/tilemap.gd
@@ -10,10 +10,12 @@ extends TileMapLayer
 #endregion
 
 func _use_tile_data_runtime_update(coords):
-	return coords in get_used_cells()
+	if coords in get_used_cells():
+		if has_collision_at(coords): # THIS FUCKING LINE
+			return true
+	return false
 
 func _tile_data_runtime_update(coords, tile_data):
 	if coords in get_used_cells():
-		for layer in range(2):  # Itera sobre as camadas
-			if tile_data.get_collision_polygons_count(layer):  # Verifica se há colisão na camada
-				tile_data.set_navigation_polygon(layer, null)  # Remove o polígono de navegação
+		tile_data.set_navigation_polygon(0, null)
+		tile_data.set_navigation_polygon(1, null)
