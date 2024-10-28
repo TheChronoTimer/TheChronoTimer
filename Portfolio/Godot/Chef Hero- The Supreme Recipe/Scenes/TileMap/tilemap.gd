cc0196c026ae839cbbfae5f9c70b1cb2ce3f18bb
TheChronoTimer <nicolas_bauermann@tutanota.com>
2024-10-28 21:10:53 +0000

    .

--- Portfolio/Godot/Chef Hero- The Supreme Recipe/Scenes/TileMap/tilemap.gd
+++ Portfolio/Godot/Chef Hero- The Supreme Recipe/Scenes/TileMap/tilemap.gd
@@ -1,15 +1,21 @@
 extends TileMapLayer
 
+#region Var
+#region Funcionamento
+@onready var Ground = $Ground
+#endregion
+#endregion
+
 func _ready():
 	for layer in get_children():
 		if layer.name != "Ground":
 			for tile in layer.get_tiles():
 				if tile.has_collision():
-					set_navigation_layer_enabled(0, false)
-					set_navigation_layer_enabled(1, false)
+					Ground.set_navigation_layer(0, false)
+					Ground.set_navigation_layer(1, false)
 				else:
-					set_navigation_layer_enabled(0, tile.has_collision())
-					set_navigation_layer_enabled(1, tile.has_collision())
+					Ground.set_navigation_layer(0, tile.has_collision())
+					Ground.set_navigation_layer(1, tile.has_collision())
 
 func _process(_delta):
 	pass
