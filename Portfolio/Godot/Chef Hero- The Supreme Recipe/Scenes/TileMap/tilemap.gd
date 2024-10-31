a5c61fb8171287914a620535a58c2957e97fb0cb
TheChronoTimer <nicolas_bauermann@tutanota.com>
2024-10-31 11:45:25 +0000

    Avoiding Obstacles: Part 1

--- Portfolio/Godot/Chef Hero- The Supreme Recipe/Scenes/TileMap/tilemap.gd
+++ Portfolio/Godot/Chef Hero- The Supreme Recipe/Scenes/TileMap/tilemap.gd
@@ -13,20 +13,13 @@ var navigation_layers = [0, 1]
 
 func _use_tile_data_runtime_update(coords):
 	if coords in get_used_cells():
-		if _has_collision_at(coords):
-			return true
+		return true
 	return false
 
 func _tile_data_runtime_update(coords, tile_data):
 	if coords in get_used_cells():
-		for layer in navigation_layers:
-			tile_data.set_navigation_polygon(layer, null)
-
-func _has_collision_at(coords):
-	# Ver filhos
-	var tile_data = get_cell_tile_data(local_to_map(coords))
-	print("Tile ID: ", tile_data.get_id())
-	for layer in collision_layers:
-		if tile_data.get_collision_polygons_count(layer) > 0: # RETORNANDO SEMPRE 0
-			return true
-	return false
+		for n_layer in navigation_layers:
+			for c_layer in collision_layers:
+				if tile_data.get_collision_polygons_count(c_layer) > 0:
+					tile_data.set_navigation_polygon(n_layer, null)
+# Falta recursividade (children)
