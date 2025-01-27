f68f28e45b6586826b93260b1ba175e4123157e0
TheChronoTimer <nicolas_bauermann@tutanota.com>
2025-01-27 09:26:20 +0000

    Sync Changes

--- Portfolio/Godot/Chef Hero- The Supreme Recipe/Scenes/TileMap/tilemap.gd
+++ Portfolio/Godot/Chef Hero- The Supreme Recipe/Scenes/TileMap/tilemap.gd
@@ -1,7 +1,20 @@
 extends TileMapLayer
 
 func _ready():
-	pass
-	
-func _process(delta):
-	pass
+	# Tamanho do retângulo usado pelo TileMap
+	var used_rect = get_used_rect()
+
+	# Itera sobre todas as coordenadas usadas no TileMap
+	for x in range(used_rect.position.x, used_rect.end.x):
+		for y in range(used_rect.position.y, used_rect.end.y):
+			
+			# Itera sobre os filhos do TileMap
+			for child in get_children():
+				if child is TileMapLayer:  # Garante que o filho é um TileMap
+					var child_cell = child.get_cell_tile_data(Vector2(x, y))  # Obtém o ID do tile na coordenada
+					if child_cell:
+						if child_cell.get_collision_polygons_count(0) > 0:  # Se o tile existir, há colisão
+							# Define o estado de "não caminhável" no layer Ground
+							var cell_data = self.get_cell_tile_data(Vector2(x, y))
+							if cell_data:  # Verifica se existe um tile na posição atual
+								self.set_cell(Vector2i(x, y), -1)
